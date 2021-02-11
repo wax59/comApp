efc266ab7e702d7ea4491d5b6fcd68d4df1a82f9
Thomas Decatoire <thomas.decatoire@gmail.com>
2021-02-12 00:38:00 +0100

    UI and bootstrap stuff

--- front/js/main.js
+++ front/js/main.js
@@ -32,24 +32,51 @@
     return "";
   }
 
-let posts;
+let posts = [];
 
 function displayNewMessage(message){
+  console.log(posts)
   let elem = document.createElement('div');
         elem.id = message._id
-        elem.innerHTML =    `<li class="list-group-item">
-                             <div class="messageTitle" id="${message._id}"> ${message.title} (${message.creator}) 
-                                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${message._id}">&#10149;</button>
+        elem.innerHTML =    `<li class="list-group-item mb-1">
+                              <div>
+                                <span class="fs-5 messageTitle" id="${message._id}"> </span>
+                                <button class="btn btn-primary btn-sm float-start me-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${message._id}">&#10149;</button>
+                                <button class="btn btn-danger btn-sm deleteButton float-end" id="${message._id}">Delete</button>
+                                <button class="btn btn-secondary btn-sm editButton float-end" id="${message._id}">Edit</button>
                              </div>
-                             <div class="collapse show collapse-message" id="collapse${message._id}">
+                             <div class="collapse show collapse-message border-top border-1 mt-1" id="collapse${message._id}">
                               <div class="messageMessage" id="${message._id}">${message.message}</div>
-                              <div><button class="btn btn-secondary editButton" id="${message._id}">Edit</button>
-                              <button class="btn btn-danger deleteButton" id="${message._id}">Delete</button></div>
                              </div>
                              </li>`;
         document.getElementById('posts').appendChild(elem);
         addDeleteButtonLogic(message._id);
         addEditButtonLogic(message._id);
+        updateTitle(message)
+}
+
+function updateTitle(message){
+  let diff = 0;
+  if(message.lastModifyDate){
+    modify = new Date(message.lastModifyDate);
+    diff = Math.floor((new Date() - modify) / 1000 / 60);
+    if (diff > 59){
+      min = diff % 60;
+      hour = Math.floor(diff / 60);
+      $( `#${message._id}.messageTitle` ).html(`${message.title} <span class="fst-italic fs-6">(${message.creator}) - ${message.createdAt.substring(3, 21)} - modified ${hour}h${min}min ago</span>`);
+    } else {
+      $( `#${message._id}.messageTitle` ).html(`${message.title} <span class="fst-italic fs-6">(${message.creator}) - ${message.createdAt.substring(3, 21)} - modified ${diff}min ago</span>`);
+    }
+  } else {
+    diff = Math.floor((new Date() - new Date(message.createdAt)) / 1000 / 60);
+    if (diff > 59){
+      min = diff % 60;
+      hour = Math.floor(diff / 60);
+      $( `#${message._id}.messageTitle` ).html(`${message.title} <span class="fst-italic fs-6">(${message.creator}) - ${message.createdAt.substring(3, 21)} - created ${hour}h${min}min ago</span>`);
+    } else {
+      $( `#${message._id}.messageTitle` ).html(`${message.title} <span class="fst-italic fs-6">(${message.creator}) - ${message.createdAt.substring(3, 21)} - created ${diff}min ago</span>`);
+    }
+  }
 }
 
 function addDeleteButtonLogic(id){
@@ -64,10 +91,15 @@ function deleteMessage(id) {
 
 function addEditButtonLogic(id){
   $( `#${id}.editButton` ).click(function(el) {
-    $( `#${id}.messageMessage` )
-    var divHtml = $(`#${id}.messageMessage`).html().replace(/<br>/g, '\n');
-    //var editableText = $(`<textarea class="messageMessage" id="${id}" />`);
-    var editableText = $(`<textarea class="form-control messageMessage" id="${id}" rows="3" />`);
+    if ( $( `#collapse${id}` ).hasClass('show') != true){
+      $( `#collapse${id}` ).addClass("show");
+    }
+    let divHtml = $(`#${id}.messageMessage`).html().replace(/<br>/g, '\n');
+    let nbLines = 0;
+    if(divHtml.match(/\n/g)) {
+      nbLines = divHtml.match(/\n/g).length; 
+    }
+    let editableText = $(`<textarea class="form-control messageMessage" id="${id}" rows="${nbLines + 3}" />`);
     editableText.val(divHtml);
     $(`#${id}.messageMessage`).replaceWith(editableText);
     editableText.focus();
@@ -121,18 +153,23 @@ socket.on('deleted message', function(msg) {
 
 socket.on('updated message', function(msg) {
   $( `#${msg._id}.messageMessage` ).html(msg.message);
+  updateTitle(msg)
 })
 
+
+//DAS save and load functions
 $( "#saveDas" ).click(function(event) {
-  console.log('here')
   let das = $( "#das" ).val()
   document.cookie = `das=${das}`;
   event.preventDefault();
-  console.log('here2')
 })
 
 $( document ).ready(function() {
   let das = getCookie("das");
-  console.log(das)
   $( "#das" ).val(das);
 });
+
+//Auto Update Title every minute
+setInterval(function() {
+  posts.forEach(message => updateTitle(message))
+}, 60 * 1000);
